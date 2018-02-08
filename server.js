253bdf9f923a68c516b36542c04a2c34848e7b9d
dssd <gonza.edelp77@gmail.com>
2018-02-08 17:40:44 -0300

    Cambiando /user

--- server.js
+++ server.js
@@ -250,7 +250,7 @@ app.get('/logout', function(req, res){
   res.clearCookie('remember_me');
   users[0]=null;
   req.logout();
-  //res.redirect('/');
+  res.redirect('/');
 });
 
 app.listen(process.env.PORT, function() {
