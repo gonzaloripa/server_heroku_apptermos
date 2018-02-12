7995dc1cb037030c1cb84b90b699bfce216836e9
dssd <gonza.edelp77@gmail.com>
2018-02-12 17:17:46 -0300

    probando gdrive

--- server.js
+++ server.js
@@ -1,125 +1,66 @@
-var fs = require('fs');
-var readline = require('readline');
-var google = require('googleapis');
 var googleAuth = require('google-auth-library');
-
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/drive-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-    process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'drive-nodejs-quickstart.json';
-
-// Load client secrets from a local file.
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the
-  // Drive API.
-  authorize(JSON.parse(content), listFiles);
+var google = require('googleapis');
+var fs = require('fs');
+var OAuth2 = google.auth.OAuth2;
+var CLIENT_ID =   "439187847005-f4eoorejh555cpg4uu36cas1j3deg9ce.apps.googleusercontent.com";
+var CLIENT_SECRET = "63vJPjBQTAKQv4ImMH76FA82";
+var REDIRECT_URL = "https://frozen-everglades-78768.herokuapp.com/";
+var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+var drive = google.drive({ version: 'v3', auth: oauth2Client });
+// generate a url that asks permissions for Google+ and Google Calendar scopes
+var scopes = [
+"https://www.googleapis.com/auth/drive",
+"https://www.googleapis.com/auth/drive.file" 
+];
+ 
+var url = oauth2Client.generateAuthUrl({
+  access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
+  scope: scopes // If you only need one scope you can pass it as string
 });
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
-    }
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
+console.log(url); //this is the url which will authenticate user and redirect to your local server. copy this and paste into browser
+ 
+module.exports.googleDrive = (function () {
+  var getTokens = function(code) {
+    oauth2Client.getToken(code, function(err, tokens) {
+      // Now tokens contains an access_token and an optional refresh_token. Save them.
+      if(!err) {
+        console.log("tokens: ",tokens);
+        //refresh_token
+        oauth2Client.setCredentials(tokens);
+            return createFolder('test',null,next);
+        }
+      else {
+        console.log("error: ",err);
       }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
     });
-  });
-}
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
+  };
+ 
+ 
+//creating the folder in drive
+  function createFolder(name,folderId,next) {
+    var folderIds=[];
+    if(folderId !== null){
+      folderIds.push(folderId);
     }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
-}
-
-/**
- * Lists the names and IDs of up to 10 files.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listFiles(auth) {
-  var service = google.drive('v3');
-  service.files.list({
-    auth: auth,
-    pageSize: 10,
-    fields: "nextPageToken, files(id, name)"
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    var files = response.files;
-    if (files.length == 0) {
-      console.log('No files found.');
-    } else {
-      console.log('Files:');
-      for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        console.log('%s (%s)', file.name, file.id);
+    var fileMetadata = {
+      'name' : name,
+      'mimeType' : 'application/vnd.google-apps.folder',
+       parents: folderIds
+    };
+    drive.files.create({
+      resource: fileMetadata,
+      fields: 'id'
+    }, function(err, file) {
+      if(err) {
+        console.log("error creating folder: ",err);
+        next(err);
+      } else {
+        console.log('Folder Id: ', file.id);
+        next(err,file.id);
       }
-    }
-  });
-}
+    });
+  }
+  return {
+    getTokens:getTokens
+  };
+})();
