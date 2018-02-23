366d6b700ca5147cadcde53d366c38ef5f1af389
dssd <gonza.edelp77@gmail.com>
2018-02-23 18:04:54 -0300

    drive

--- server.js
+++ server.js
@@ -448,18 +448,20 @@ app.get('/login', function(req, res){
 });
 
 app.get('/', function(req, res){
-   if(req.query){
-      res.render('index', { user: req.query, message: req.flash('error') });
-   }
-   else{
+   
       if(req.user){
        console.log("-------Request User del /: "+ req.user);
        res.render('index', { user: req.user, message: req.flash('error') });
       }
       else{
-        var userObj= {username:'lauchagnr'};
-        console.log("-------Request User del /: "+ userObj.username);
-        res.render('index', { user: userObj, message: req.flash('error') });
+        if(req.query.username){
+          console.log("-------Request User del /: "+ req.query);
+          res.render('index', { user: req.query, message: req.flash('error') });
+        }else{
+          var userObj= {username:'lauchagnr'};
+          console.log("-------Request User del /: "+ userObj.username);
+          res.render('index', { user: userObj, message: req.flash('error') });
+        }
       }
    }
 });
