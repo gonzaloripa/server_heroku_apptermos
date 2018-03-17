25a0ed4a1b5c5f8875c55dbe1104b1a2c20b68ac
dssd <gonza.edelp77@gmail.com>
2018-03-17 20:10:13 -0300

    drive

--- server.js
+++ server.js
@@ -825,8 +825,22 @@ app.get('/login', function(req, res){
 });
 
 app.get('/', function(req, res){
-   
-      res.redirect('/login');
+         res.redirect('/login');
+
+      /*if(req.user){
+       console.log("-------Request User del /: "+ req.user);
+       res.render('index', { user: req.user, message: req.flash('error') });
+      }
+      else{
+        if(req.query.username){
+          console.log("-------Request User del /: "+ req.query);
+          res.render('index', { user: req.query, message: req.flash('error') });
+        }else{
+          var userObj= {username:'admin'};
+          console.log("-------Request User del /: "+ userObj.username);
+          res.render('index', { user: userObj, message: req.flash('error') });
+        }
+      }*/
    
 });
 
